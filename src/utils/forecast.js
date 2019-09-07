21f784773233e8c4271631ba54681441bf8fc0e9
sreekanth korampally <sreekanthkorampally@sreekanths-mbp.home>
2019-09-06 22:47:22 -0400

    works fine need to update the styles though

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,7 +1,7 @@
 const request = require('request')
 
 const forecast = (latitude, longitude, callback) => {
-    const url = 'https://api.darksky.net/forecast/9d1465c6f3bb7a6c71944bdd8548d026/' + latitude + ',' + longitude
+    const url = 'https://api.darksky.net/forecast/1d621c24f15e133eec8fee878267c17d/' + latitude + ',' + longitude
 
     request({ url, json: true }, (error, { body }) => {
         if (error) {
--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -1,7 +1,7 @@
 const request = require('request')
 
 const geocode = (address, callback) => {
-    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + address + '.json?access_token=pk.eyJ1IjoiYW5kcmV3bWVhZDEiLCJhIjoiY2pvOG8ybW90MDFhazNxcnJ4OTYydzJlOSJ9.njY7HvaalLEVhEOIghPTlw&limit=1'
+    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + address + '.json?access_token=pk.eyJ1Ijoic3Jpa2FudGg2MDA2IiwiYSI6ImNrMDhxbXBnYjAwdTQzY3ViZHJwMXE0cTIifQ.84jUpigD104eLl_d-IKm4Q&limit=1'
 
     request({ url, json: true }, (error, { body }) => {
         if (error) {
